bda56d60e9cde732441343dc2654069a02a8fb48
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-12-26 17:10:36 +0800

    Improve epic sorting

--- lib/sprint_reporter/markdown_renderer.rb
+++ lib/sprint_reporter/markdown_renderer.rb
@@ -13,7 +13,15 @@ module SprintReporter
 
       epics = @items.group_by { |it| it[:epic] }
 
-      epics.each.map do |epic, items|
+      defined_keys = @epics ? @epics.keys : []
+      undefined_keys = epics.keys - defined_keys
+      keys = defined_keys + undefined_keys
+
+      # Move `nil` to the end
+      keys = keys.sort_by { |epic| epic.nil? ? 1 : 0 }
+
+      keys.map do |epic|
+        items = epics[epic]
         render_epic(epic, items)
       end.join("\n\n")
     end
@@ -26,7 +34,9 @@ module SprintReporter
     end
 
     def render_epic_heading(epic)
-      if epic
+      if epic && @epics && @epics[epic]
+        "## #{@epics[epic]}"
+      elsif epic
         "## #{epic}"
       else
         "## Other"
--- test/markdown_renderer_test.rb
+++ test/markdown_renderer_test.rb
@@ -1,16 +1,16 @@
 require 'test_helper'
 
 describe 'MarkdownRenderer' do
-  it 'works' do
-    epics = nil
-
+  it 'works with epics' do
     items = [
       { key: 'PR-12', type: 'Feature', title: 'Sign up', epic: 'PR-2' },
       { key: 'PR-13', type: 'Feature', title: 'Sign out', epic: 'PR-2' },
       { key: 'PR-14', type: 'Feature', title: 'Write posts', epic: 'PR-3' },
+      { key: 'PR-16', type: 'Feature', title: 'Deployment', epic: nil },
       { key: 'PR-15', type: 'Feature', title: 'Make comments', epic: 'PR-3' },
       { key: 'PR-17', type: 'Bug', title: 'Log in not working', epic: 'PR-2' },
-      { key: 'PR-16', type: 'Feature', title: 'Deployment', epic: nil }
+      { key: 'PR-18', type: 'Feature', title: 'Podcast download', epic: 'PR-4' },
+      { key: 'PR-19', type: 'Feature', title: 'Podcast listen', epic: 'PR-4' },
     ]
 
     epics = {
@@ -25,6 +25,47 @@ describe 'MarkdownRenderer' do
     ).render
 
     expect(output.strip).must_equal <<~END.strip
+      ## Blogging
+
+      - [`PR-14`](https://foo.atlassian.net/browse/PR-14) Write posts
+      - [`PR-15`](https://foo.atlassian.net/browse/PR-15) Make comments
+
+      ## User management
+
+      - [`PR-12`](https://foo.atlassian.net/browse/PR-12) Sign up
+      - [`PR-13`](https://foo.atlassian.net/browse/PR-13) Sign out
+      - [`PR-17`](https://foo.atlassian.net/browse/PR-17) Log in not working
+
+      ## PR-4
+
+      - [`PR-18`](https://foo.atlassian.net/browse/PR-18) Podcast download
+      - [`PR-19`](https://foo.atlassian.net/browse/PR-19) Podcast listen
+
+      ## Other
+
+      - [`PR-16`](https://foo.atlassian.net/browse/PR-16) Deployment
+    END
+  end
+
+  it 'works without epics' do
+    items = [
+      { key: 'PR-12', type: 'Feature', title: 'Sign up', epic: 'PR-2' },
+      { key: 'PR-13', type: 'Feature', title: 'Sign out', epic: 'PR-2' },
+      { key: 'PR-14', type: 'Feature', title: 'Write posts', epic: 'PR-3' },
+      { key: 'PR-16', type: 'Feature', title: 'Deployment', epic: nil },
+      { key: 'PR-15', type: 'Feature', title: 'Make comments', epic: 'PR-3' },
+      { key: 'PR-17', type: 'Bug', title: 'Log in not working', epic: 'PR-2' },
+      { key: 'PR-18', type: 'Feature', title: 'Podcast download', epic: 'PR-4' },
+      { key: 'PR-19', type: 'Feature', title: 'Podcast listen', epic: 'PR-4' },
+    ]
+
+    output = SprintReporter::MarkdownRenderer.new(
+      items,
+      domain: 'foo.atlassian.net',
+      epics: nil
+    ).render
+
+    expect(output.strip).must_equal <<~END.strip
       ## PR-2
 
       - [`PR-12`](https://foo.atlassian.net/browse/PR-12) Sign up
@@ -35,6 +76,11 @@ describe 'MarkdownRenderer' do
 
       - [`PR-14`](https://foo.atlassian.net/browse/PR-14) Write posts
       - [`PR-15`](https://foo.atlassian.net/browse/PR-15) Make comments
+
+      ## PR-4
+
+      - [`PR-18`](https://foo.atlassian.net/browse/PR-18) Podcast download
+      - [`PR-19`](https://foo.atlassian.net/browse/PR-19) Podcast listen
 
       ## Other
 
