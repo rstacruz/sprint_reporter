992b649497c06d10b32053d44767af3bdfc9b724
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-01-11 19:38:52 +0800

    Parse links

--- lib/sprint_reporter/jira_parser.rb
+++ lib/sprint_reporter/jira_parser.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 module SprintReporter
   module JiraParser
     module_function
@@ -21,8 +23,20 @@ module SprintReporter
         type: row['Issue Type'], # => 'Bug'
         status: row['Status'], # => 'Closed'
         epic: row['Custom field (Epic Link)'], # => 'PROJ-111'
-        priority: row['Priority'] # => 'Highest'
+        priority: row['Priority'], # => 'Highest'
+        links: get_links(row)
       }
+    end
+
+    def get_links(row)
+      desc = row['Description'] || ''
+      urls = desc.scan(/https?:\/\/[A-Za-z0-9\.\/\?&%=\+]+/) || []
+
+      urls.map do |url|
+        uri = URI.parse(url)
+        host = uri.host
+        { host: host, url: url }
+      end
     end
   end
 end
--- test/jira_parser_test.rb
+++ test/jira_parser_test.rb
@@ -1,26 +1,37 @@
 require 'test_helper'
 
 describe 'Jira Parser' do
-  let :fixture_path do
-    File.expand_path('../../example/JIRA.csv', __FILE__)
-  end
+  describe 'fixture' do
+    let :fixture_path do
+      File.expand_path('../../example/JIRA.csv', __FILE__)
+    end
 
-  let :items do
-    SprintReporter::JiraParser.get_items(fixture_path)
-  end
+    let :items do
+      SprintReporter::JiraParser.get_items(fixture_path)
+    end
 
-  it 'works' do
-    expect(items.length).must_equal 8
-  end
+    it 'works' do
+      expect(items.length).must_equal 8
+    end
 
-  it 'has the correct first item' do
-    expect(items[0]).must_equal(
-      title: 'Sign up',
-      key: 'PR-12',
-      type: 'Feature',
-      status: 'Closed',
-      epic: 'PR-2',
-      priority: 'Medium'
-    )
+    it 'has the correct first item' do
+      expect(items[0]).must_equal(
+        title: 'Sign up',
+        key: 'PR-12',
+        type: 'Feature',
+        status: 'Closed',
+        epic: 'PR-2',
+        priority: 'Medium',
+        links: []
+      )
+    end
+
+    it 'parses trello URLs' do
+      item = items.select { |it| it[:key] == 'PR-19' }.first
+
+      expect(item[:links]).must_equal([
+        { host: 'trello.com', url: 'https://trello.com/card/foobar' }
+      ])
+    end
   end
 end
